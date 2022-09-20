441aac01bce3b3764ee07c51526d87d94c9e09dd
novioll <novioll@mail.ru>
2022-09-20 23:59:34 +0300

    ëxample1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] array = {"Hello", "2", "world", ":-)"};
+var array2 = new string[array.Length];
+int i = 0;
+foreach (var element in array)
+{
+    if (element.Length <= 3)
+    {
+        array2[i] = element; Console.Write(array2[i] + ", "); i++;
+    }
+}
